da67a572731d97cdc08bb153f4e47c6e27b0546a
Shahzad Akhtar <125475371+Shahzadkhan786@users.noreply.github.com>
2023-09-28 11:44:54 +0500

    Create ecosystem.config.js
    Signed-off-by: Shahzad Akhtar <125475371+Shahzadkhan786@users.noreply.github.com>

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'construction-site',        // Name of your application
+      script: 'index.js',    // Path to your entry script
+      instances: 1,          // Number of instances to run (adjust as needed)
+      autorestart: true,     // Enable auto-restart on failure
+      watch: false,          // Enable file watching for changes
+      max_memory_restart: '100M', // Maximum memory usage for auto-restart
+      env: {
+        NODE_ENV: 'production', // Set the environment to production
+      },
+      env_production: {
+        NODE_ENV: 'production', // Environment settings for production
+      },
+    },
+  ],
+};
